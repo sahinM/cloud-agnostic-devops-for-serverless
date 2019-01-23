4f61d2ab884a365cdc581005f88fcc139aa0f6ab
Müslüm Sahin <sahin.muesluem@web.de>
2019-01-23 22:01:57 +0100

    changed Travis CI config

--- terraform-resources/aws/eks/main.tf
+++ terraform-resources/aws/eks/main.tf
@@ -1,13 +1,50 @@
-module "module" {
-  source  = "WesleyCharlesBlake/eks/aws"
-  version = "1.0.5"
-
-  cluster-name       = "${var.cluster-name}"
-  aws-region         = "${var.aws-region}"
-  k8s-version        = "${var.k8s-version}"
-  node-instance-type = "${var.node-instance-type}"
-  desired-capacity   = "${var.desired-capacity}"
-  max-size           = "${var.max-size}"
-  min-size           = "${var.min-size}"
-  vpc-subnet-cidr    = "${var.vpc-subnet-cidr}"
+terraform {
+  required_version = ">= 0.11.8"
 }
+
+provider "aws" {
+  region  = "${var.region}"
+}
+
+provider "random" {
+  version = "= 1.3.1"
+}
+
+# access to the list of AWS Availability Zones
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "OpenFaaS-VPC"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+module "eks" {
+  source       = "terraform-aws-modules/eks/aws"
+  cluster_name = "OpenFaaS-Demo-Cluster"
+  subnets      = ["${module.vpc.private_subnets}"]
+  vpc_id       = "${module.vpc.vpc_id}"
+  worker_groups = [
+    {
+      instance_type = "m4.large"
+      asg_max_size  = 5
+    }
+  ]
+  tags = {
+    environment = "test"
+  }
+}
+
