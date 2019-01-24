6795b536e9d38b87fb0a1e10e0154c3e8ba8caa7
Müslüm Sahin <sahin.muesluem@web.de>
2019-01-24 21:24:31 +0100

    changed Travis CI config

--- locals.tf
+++ locals.tf
@@ -1,3 +1,3 @@
 locals {
-    project_name = "CloudAgnosticCDForServerless"
+    project_name = "cloud-agnostic-cd-or-serverless"
 }
--- remote-backend/backend.tf
+++ remote-backend/backend.tf
@@ -1,8 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "${var.backend_bucket_name}"
     key            = "terraform.tfstate"
     region         = "${var.backend_region}"
-    dynamodb_table = "${var.backend_table_name"}"
   }
 }
