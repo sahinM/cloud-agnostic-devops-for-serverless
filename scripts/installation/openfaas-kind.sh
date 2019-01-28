cc5a174e6e5bfc1ba6f40daaa1d105208000c146
Müslüm Sahin <sahin.muesluem@web.de>
2019-01-28 20:16:55 +0100

    changed Travis CI config

--- scripts/installation/openfaas-kind.sh
+++ scripts/installation/openfaas-kind.sh
@@ -6,21 +6,21 @@ go get sigs.k8s.io/kind
 kind create cluster --name openfaas
 export KUBECONFIG="$(kind get kubeconfig-path --name="openfaas")"
   # create service account with rbac permissions for tiller 
-kubectl -n kube-system create sa tiller
-    && kubectl create clusterrolebinding tiller
-          --clusterrole cluster-admin
-          --serviceaccount=kube-system:tiller
+kubectl -n kube-system create sa tiller \
+    && kubectl create clusterrolebinding tiller \
+          --clusterrole cluster-admin \
+          --serviceaccount=kube-system:tiller \
   # installing server component of helm -> tiller 
 helm init --skip-refresh --wait --upgrade --service-account tiller
   # adding namespaces openfaas and openfaas-fn
 kubectl apply -f https://raw.githubusercontent.com/openfaas/faas-netes/master/namespaces.yml
   # installing openFaas with Helm
-helm repo add openfaas https://openfaas.github.io/faas-netes &&
-        helm repo update &&
-        helm upgrade openfaas --wait --install openfaas/openfaas
-          --namespace openfaas
-          --set basic_auth=false
-          --set functionNamespace=openfaas-fn
+helm repo add openfaas https://openfaas.github.io/faas-netes && \
+        helm repo update && \
+        helm upgrade openfaas --wait --install openfaas/openfaas \
+          --namespace openfaas \
+          --set basic_auth=false \
+          --set functionNamespace=openfaas-fn \
           --set operator.create=true
   # Port forwarding to localhost:8080 as background process
 kubectl port-forward svc/gateway -n openfaas 8080:8080 &
