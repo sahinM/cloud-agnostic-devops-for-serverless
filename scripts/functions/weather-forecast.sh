d8fdca77cefe00f19fc46e62f9fe7d04fa70b32b
Müslüm Sahin <sahin.muesluem@web.de>
2019-01-30 01:54:50 +0100

    changed Travis CI config

--- scripts/functions/weather-forecast.sh
+++ scripts/functions/weather-forecast.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # build, deploy and push weather-forecast function and call it
+export KUBECONFIG="$(kind get kubeconfig-path --name="openfaas")"
 cd testing
 faas-cli up -f weather-forecast.yml
 # wait_for_service_to_start echo
