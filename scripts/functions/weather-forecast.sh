f6a2962adecb4a8f95ba95b6a3ba6b8e03e0bf40
Müslüm Sahin <sahin.muesluem@web.de>
2019-02-13 16:19:10 +0100

    changed Travis CI config

--- scripts/functions/weather-forecast.sh
+++ scripts/functions/weather-forecast.sh
@@ -3,8 +3,10 @@
 # build, deploy and push weather-forecast function and call it
 export KUBECONFIG="$(kind get kubeconfig-path --name="openfaas")"
 cd testing
-faas-cli build -f weather-forecast.yml
-faas-cli deploy -f weather-forecast.yml
+faas-cli up -f weather-forecast.yml
 
+# Some test cases
 echo -n Stuttgart, DE | faas-cli invoke weather-forecast
+echo -n Istanbul | faas-cli invoke weather-forecast
+echo -n Heilbronn:DE | faas-cli invoke weather-forecast
 cd -
