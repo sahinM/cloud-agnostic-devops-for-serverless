89bf277bf4ead3dacd1cca54f3ba24dfe2ef5e76
Müslüm Sahin <sahin.muesluem@web.de>
2019-02-13 18:16:54 +0100

    changed Travis CI config

--- scripts/deployment/deploy_production.sh
+++ scripts/deployment/deploy_production.sh
@@ -3,5 +3,6 @@
 echo "$DOCKER_PW" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 # Push to the production environment
+faas-cli build -f testing/weather-forecast.yml
 faas-cli push -f testing/weather-forecast.yml --tag production
 faas-cli deploy -f testing/weather-forecast.yml
