93cda089e8a6a7c76dc046f1c0f6cdd3e1f4097e
Müslüm Sahin <sahin.muesluem@web.de>
2019-02-14 16:50:57 +0100

    changed Travis CI config

--- scripts/deployment/deploy_staging.sh
+++ scripts/deployment/deploy_staging.sh
@@ -4,7 +4,7 @@
 export KUBECONFIG=openfaas
 kubectl get nodes
 echo "$DOCKER_PW" | docker login -u "$DOCKER_USERNAME" --password-stdin
-echo "$EKS_OF_PW" | faas-cli login --username "$EKS_OF_USER" --password-stdin
+echo "$EKS_OF_PW" | faas-cli login --username "$EKS_OF_USER" --password-stdin --gateway="$EKS_OF_GW"
 # echo 8aa43049aac76b200072a0f827b330989846bb09 | faas-cli login --username sahinm --password-stdin
 cd testing
 faas-cli up -f weather-forecast.yml --gateway="$EKS_OF_GW" --tag=branch
