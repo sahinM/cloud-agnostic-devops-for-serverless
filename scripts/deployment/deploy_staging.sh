098cc63772a9ee0ef221b405839064bea40ca356
Müslüm Sahin <sahin.muesluem@web.de>
2019-02-14 02:47:04 +0100

    changed Travis CI config

--- scripts/deployment/deploy_staging.sh
+++ scripts/deployment/deploy_staging.sh
@@ -4,7 +4,7 @@ export KUBECONFIG=kubeconfig-eks-openfaas-staging-cluster
 echo "$DOCKER_PW" | docker login -u "$DOCKER_USERNAME" --password-stdin
 cd app
 echo $EKS_OF_PW | faas-cli login --username $EKS_OF_USER --password-stdin
-faas-cli up -f weather-forecast.yml --tag=branch
+faas-cli up -f app/weather-forecast.yml --tag=branch
 
 # Some test cases
 echo -n Stuttgart, DE | faas-cli invoke weather-forecast
