2f96139d678f5227864d75709838dc0fdf52ab1a
Müslüm Sahin <sahin.muesluem@web.de>
2019-02-14 02:27:04 +0100

    changed Travis CI config

--- scripts/deployment/deploy_production.sh
+++ scripts/deployment/deploy_production.sh
@@ -3,4 +3,4 @@
 echo "$DOCKER_PW" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 # Push to the production environment
-faas-cli up -f weather-forecast.yml --tag=branch --gateway
+faas-cli up -f weather-forecast.yml --tag=branch
--- scripts/functions/weather-forecast.sh
+++ scripts/functions/weather-forecast.sh
@@ -2,9 +2,9 @@
 
 # build, deploy and push weather-forecast function and call it
 export KUBECONFIG="$(kind get kubeconfig-path --name="openfaas")"
-cd testing
+cd app
 # building and testing the function with gradle tool
-faas-cli up -f weather-forecast.yml --tag=branch
+faas-cli up -f weather-forecast.yml
 
 # Some test cases
 echo -n Stuttgart, DE | faas-cli invoke weather-forecast
